05678bbaaf6c6183075896b476075ab2daed997e
sydorenkovd <sydorenkovd@gmail.com>
2017-08-07 22:56:35 +0300

    crud for rest

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -15,7 +15,9 @@ class ProgrammerController extends BaseController
     protected function addRoutes(ControllerCollection $controllers)
     {
         $controllers->post('/api/programmers', array($this, 'newAction'));
-    }
+        $controllers->get('/api/programmers/{nickname}', array($this, 'showAction'))
+            ->bind('api_programmers_show');
+        $controllers->get('/api/programmers', array($this, 'listAction'));}
 
     public function newAction(Request $request) {
         $data = json_decode($request->getContent(), true);
@@ -27,10 +29,47 @@ class ProgrammerController extends BaseController
         $model->userId = $this->findUserByUsername('sydorenkovd')->id;
 
         $this->save($model);
-
-        $response = new Response('works fo sure', 201);
-        $response->headers->set('Location', '/programmers/locatiomn');
+        $programmerUrl = $this->generateUrl(
+            'api_programmers_show',
+            ['nickname' => $model->nickname]
+        );
+        $response = new Response(json_encode($model), 201);
+        $response->headers->set('Location', $programmerUrl);
 
         return $response;
+    }
+    public function showAction($nickname)
+    {
+        $programmer = $this->getProgrammerRepository()->findOneByNickname($nickname);
+        if (!$programmer) {
+            $this->throw404('Crap! This programmer has deserted! We\'ll send a search party');
+        }
+        $data = $this->serializeProgrammer($programmer);
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    public function listAction() {
+        $programmers = $this->getProgrammerRepository()->findAll();
+        $data = array('programmers' => array());
+        foreach ($programmers as $programmer) {
+            $data['programmers'][] = $this->serializeProgrammer($programmer);
+        }
+
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+    private function serializeProgrammer(Programmer $programmer)
+    {
+        return array(
+            'nickname' => $programmer->nickname,
+            'avatarNumber' => $programmer->avatarNumber,
+            'powerLevel' => $programmer->powerLevel,
+            'tagLine' => $programmer->tagLine,
+        );
     }
 }
